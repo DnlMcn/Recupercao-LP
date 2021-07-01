9bae4e0d54163803a9b39ebe183ad98724f2b38a
Danilo Mancini <d.manciniqueiroz@gmail.com>
2021-06-30 23:03:30 -0300

    Adicionar Movement.cs
    Movimentação no eixo X teoreticamente funcional.

---
+++ Recuperacão LP/Assets/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float accelCoeff;
+    float currentSpeed;
+    float deltaPosition;
+    float accelerationForce;
+    Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+
+        currentSpeed = transform.position.x - deltaPosition;
+        bool isForward = currentSpeed >= 0 ? true : false;
+
+        accelerationForce = Mathf.Lerp(accelCoeff, 1, Mathf.Clamp01(currentSpeed));
+
+        if (v > 0) 
+        {
+            if (isForward) {rb.AddForce(new Vector3(v * accelerationForce, 0, 0), ForceMode.Force);}
+            else {rb.AddForce(new Vector3(v * accelerationForce * 2, 0, 0), ForceMode.Force);}
+        }
+
+        if (v < 0) 
+        {
+            if (!isForward) {rb.AddForce(new Vector3(v * accelerationForce, 0, 0), ForceMode.Force);}
+            else {rb.AddForce(new Vector3(v * accelerationForce * 2, 0, 0), ForceMode.Force);}
+        }
+
+        deltaPosition = transform.position.x;
+
+    }
+}
+
