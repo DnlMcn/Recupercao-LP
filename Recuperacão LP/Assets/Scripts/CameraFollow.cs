6050bc8be3d239bdbfb88e11f1e6e9bd3e1b9e25
Danilo Mancini <d.manciniqueiroz@gmail.com>
2021-07-02 11:07:20 -0300

    Revamp movement

---
+++ Recuperacão LP/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    Vector3 offsetDirection;
+    float offsetMagnitude;
+
+    void Start()
+    {
+        offsetMagnitude = Vector3.Distance(target.position, transform.position);
+
+        offsetDirection = (transform.position - target.position).normalized;
+
+    }
+
+    void Update()
+    {
+        transform.position = target.position + offsetDirection * offsetMagnitude;
+
+    }
+
+}
---
+++ Recuperacão LP/Assets/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float accelCoeff;
+    float currentSpeed;
+    Vector3 deltaPosition;
+    float accelerationForce;
+    Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        currentSpeed = Vector3.Distance(transform.position, deltaPosition);
+
+        // accelerationForce = Mathf.Lerp(20, accelCoeff, Mathf.Clamp(currentSpeed, 1, .1f));
+
+        accelerationForce = accelCoeff;
+
+        rb.AddForce(new Vector3(h, 0, v) * accelerationForce * Time.deltaTime, ForceMode.Acceleration);
+
+        deltaPosition = transform.position;
+
+        // bool isForward = currentSpeed >= 0 ? true : false;
+
+        // accelerationForce = Mathf.Lerp(accelCoeff, 1, Mathf.Clamp01(currentSpeed));
+
+        // if (v > 0) 
+        // {
+        //     if (isForward) {rb.AddForce(new Vector3(v * accelerationForce, 0, 0), ForceMode.Force);}
+        //     else {rb.AddForce(new Vector3(v * accelerationForce * 2, 0, 0), ForceMode.Force);}
+        // }
+
+        // if (v < 0) 
+        // {
+        //     if (!isForward) {rb.AddForce(new Vector3(v * accelerationForce, 0, 0), ForceMode.Force);}
+        //     else {rb.AddForce(new Vector3(v * accelerationForce * 2, 0, 0), ForceMode.Force);}
+        // }
+
+        
+
+    }
+}
+
